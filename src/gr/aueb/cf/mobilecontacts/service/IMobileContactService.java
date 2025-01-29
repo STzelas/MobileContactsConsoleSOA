b5b2974553b0d4efebf2af5608c013aed69f8989
Sot. Tzelatidis <stzelati@gmail.com>
2025-01-29 20:43:43 +0200

    Service updates, validation and controller created

--- src/gr/aueb/cf/mobilecontacts/service/IMobileContactService.java
+++ src/gr/aueb/cf/mobilecontacts/service/IMobileContactService.java
@@ -17,4 +17,7 @@ public interface IMobileContactService {
     void deleteContactById(Long id) throws ContactNotFoundException;
     MobileContact getContactById(Long id) throws ContactNotFoundException;
     List<MobileContact> getAllContacts(); // Αν δε βρεθεί κανένας απλά δίνει μία κενή λίστα. Δε χρειάζεται Exception
+
+    MobileContact getContactByPhoneNumber(String phoneNumber) throws ContactNotFoundException;
+    void deleteContactByPhoneNumber(String phoneNumber) throws ContactNotFoundException;
 }
